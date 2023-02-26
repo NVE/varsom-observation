54a7742dd7749d71143e6f9dacf1aa26e69c8894
hgr069 <hgr069@post.uit.no>
2023-02-26 20:03:36 +0100

    La til WaterLevel, DamageObs

--- observasjonskort/src/utils/ponicode/utils.test.ts
+++ observasjonskort/src/utils/ponicode/utils.test.ts
@@ -1,5 +1,7 @@
 import * as utils from "../utils"
 
+
+
 // @ponicode
 describe("utils.getGeoHazardIdFromName", () => {
     test("0", () => {
@@ -30,6 +32,8 @@ describe("utils.getGeoHazardIdFromName", () => {
         utils.getGeoHazardIdFromName("")
     })
 })
+
+
 
 // @ponicode
 describe("utils.getLangKeyFromName", () => {
@@ -67,5 +71,32 @@ describe("utils.getLangKeyFromName", () => {
 
     test("8", () => {
         utils.getLangKeyFromName("")
+    })
+})
+
+// @ponicode
+describe("utils.getDataFromApi", () => {
+    test("0", async () => {
+        await utils.getDataFromApi("object", 0, "it", "a85a8e6b-348b-4011-a1ec-1e78e9620782")
+    })
+
+    test("1", async () => {
+        await utils.getDataFromApi("string", 1, "en", "a85a8e6b-348b-4011-a1ec-1e78e9620782")
+    })
+
+    test("2", async () => {
+        await utils.getDataFromApi("number", 100, "cn", "7289708e-b17a-477c-8a77-9ab575c4b4d8")
+    })
+
+    test("3", async () => {
+        await utils.getDataFromApi("object", 0, "en", "a85a8e6b-348b-4011-a1ec-1e78e9620782")
+    })
+
+    test("4", async () => {
+        await utils.getDataFromApi("object", -100, "de", "a85a8e6b-348b-4011-a1ec-1e78e9620782")
+    })
+
+    test("5", async () => {
+        await utils.getDataFromApi("", -Infinity, "", "")
     })
 })
