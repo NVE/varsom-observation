1a5af487ff6f314c4b6cb8a100ac298c2f74e5de
jla099 <jla099@post.uit.no>
2023-03-01 20:13:55 +0100

    implemented story with language, count and type

---
+++ observasjonskort/src/components/varsom-observasjon/varsom-observation.stories.ts
@@ -0,0 +1,12 @@
+export default {   //source: https://ionic.io/blog/how-to-use-storybook-with-stencil
+    title: 'Components/VarsomObservation'
+}
+
+const Template = (args) => `<varsom-observation type="${args.type}" count=${args.count} language="${args.language}"></varsom-observation> `
+
+export const ObservationWithRegId = Template.bind({});
+    ObservationWithRegId.args = {
+        language: 'English',
+        type: 'Water',
+        count: 1
+    }
