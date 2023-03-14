3c219f9a56b9539386beb1cbed3ee694b09fb6f3
Jørgen Breistrand Lampe <jobl@nve.no>
2023-03-14 14:08:16 +0100

    small fix

--- observasjonskort/src/components/varsom-observasjon/observation-model.ts
+++ observasjonskort/src/components/varsom-observasjon/observation-model.ts
@@ -27,7 +27,7 @@ export type Observation = {
   _snowProfile?: SnowProfile,
   _landslideProblem?: LandslideProblem,
   _estimateOfRisk?: EstimateOfRisk
-  _snowSurface?: SnowSurfaceObservation,
+  _snowSurfaceObservation?: SnowSurfaceObservation,
   _dataSource?: any,
   _className?: string,
   _observationImages?: HTMLElement[],
@@ -39,7 +39,7 @@ export type Observation = {
   _waterLevel2?: WaterLevel2,
   _damageObs?: DamageObs[],
   _obsLocation?: ObsLocation,
-  _avalancheEvalProblem2?: AvalancheEvalProblem2[]
+  _avalancheEvalProblem2?: AvalancheEvalProblem2[],
   _avalancheEvaluation3?: AvalancheEvaluation3,
   _compressionTest?: CompressionTest[]
   _generalObservation?: GeneralObservation,
@@ -339,34 +339,34 @@ export type Observation = {
    }
   
    export type AvalancheEvalProblem2 = {
-    AvalProbabilityName: string,
-    AvalTriggerSimpleName: string,
-    AvalCauseDepthName: string,
-    ExposedHeightComboName:	string,
-    AvalancheExtName:	string,
-    AvalCauseName: string,
-    DestructiveSizeName: string,
-    AvalPropagationName: string,
-    AvalCauseAttributeLightName: string,
-    AvalCauseAttributeThinName:	string,
-    AvalCauseAttributeSoftName:	string,
-    AvalCauseAttributeCrystalName: string,
-    AvalProbabilityTID: number, // integer($int32) Sannsynlighet for skred. The AvalProbabilityKD unique identifier
-    AvalPropagationTID: number, //	integer($int32)
-    AvalTriggerSimpleTID: number, //	integer($int32) The AvalTriggerSimpleKD unique identifier
-    AvalCauseDepthTID: number, //	integer($int32) Hvor dypt ligger det overnevnte svake laget? The AvalCauseDepthKD unique identifier
-    ValidExposition:	string, // maxLength: 8 Velg utsatte retninger
-    ExposedHeight1: number, //	integer($int32) Øverste høyde på “utsatt høyde” symbolet.
-    ExposedHeight2: number, //	integer($int32) Nederste høyde på “utsatt høyde” symbolet.
-    ExposedHeightComboTID: number, //	integer($int32) Hvilket symbol brukes? Er utsatt tereng over ExposedHeight2 eller under den? The ExposedHeightComboKD unique identifier
-    AvalancheExtTID: number, // integer($int32) Skredtype. I appen er dette 1. felt under skredproblem. The AvalancheExtKD unique identifier
-    Comment: string, // Kommentar til skredproblemet
-    AvalCauseTID: number, // integer($int32) Hvilket svakt lag løsner skredet på? The AvalCauseKD unique identifier
-    AvalCauseAttributeLightTID: number, //	integer($int32)
-    AvalCauseAttributeThinTID: number, //	integer($int32)
-    AvalCauseAttributeSoftTID: number, //	integer($int32)
-    AvalCauseAttributeCrystalTID: number, //	integer($int32)
-    DestructiveSizeTID: number //	integer($int32) Sannsynlig tilleggsbelastning for å utløse skred. The DestructiveSizeKD unique identifier
+    AvalProbabilityName?: string,
+    AvalTriggerSimpleName?: string,
+    AvalCauseDepthName?: string,
+    ExposedHeightComboName?:	string,
+    AvalancheExtName?:	string,
+    AvalCauseName?: string,
+    DestructiveSizeName?: string,
+    AvalPropagationName?: string,
+    AvalCauseAttributeLightName?: string,
+    AvalCauseAttributeThinName?:	string,
+    AvalCauseAttributeSoftName?:	string,
+    AvalCauseAttributeCrystalName?: string,
+    AvalProbabilityTID?: number, // integer($int32) Sannsynlighet for skred. The AvalProbabilityKD unique identifier
+    AvalPropagationTID?: number, //	integer($int32)
+    AvalTriggerSimpleTID?: number, //	integer($int32) The AvalTriggerSimpleKD unique identifier
+    AvalCauseDepthTID?: number, //	integer($int32) Hvor dypt ligger det overnevnte svake laget? The AvalCauseDepthKD unique identifier
+    ValidExposition?:	string, // maxLength: 8 Velg utsatte retninger
+    ExposedHeight1?: number, //	integer($int32) Øverste høyde på “utsatt høyde” symbolet.
+    ExposedHeight2?: number, //	integer($int32) Nederste høyde på “utsatt høyde” symbolet.
+    ExposedHeightComboTID?: number, //	integer($int32) Hvilket symbol brukes? Er utsatt tereng over ExposedHeight2 eller under den? The ExposedHeightComboKD unique identifier
+    AvalancheExtTID?: number, // integer($int32) Skredtype. I appen er dette 1. felt under skredproblem. The AvalancheExtKD unique identifier
+    Comment?: string, // Kommentar til skredproblemet
+    AvalCauseTID?: number, // integer($int32) Hvilket svakt lag løsner skredet på? The AvalCauseKD unique identifier
+    AvalCauseAttributeLightTID?: number, //	integer($int32)
+    AvalCauseAttributeThinTID?: number, //	integer($int32)
+    AvalCauseAttributeSoftTID?: number, //	integer($int32)
+    AvalCauseAttributeCrystalTID?: number, //	integer($int32)
+    DestructiveSizeTID?: number //	integer($int32) Sannsynlig tilleggsbelastning for å utløse skred. The DestructiveSizeKD unique identifier
    }
   
    export type AvalancheEvaluation3 = {
