1209fd9bf7cf0848bfde846e8cdbd460b07c6c79
Jørgen Breistrand Lampe <jobl@nve.no>
2023-03-21 22:20:17 +0100

    ...

--- observasjonskort/stencil.config.ts
+++ observasjonskort/stencil.config.ts
@@ -22,6 +22,34 @@ export const config: Config = {
         src: "**/*.i18n.*.json",
         dest: "i18n"
       }],
+      
+      serviceWorker: null, // disable service workers
+    },
+    {
+      type: 'www',
+      copy: [{
+        src: "**/*icon.svg",
+        dest: "icons"
+      }],
+      
+      serviceWorker: null, // disable service workers
+    },
+    {
+      type: 'www',
+      copy: [{
+        src: "**/*.png",
+        dest: "pictures"
+      }],
+      
+      serviceWorker: null, // disable service workers
+    },
+    {
+      type: 'www',
+      copy: [{
+        src: "**/*.jpg",
+        dest: "pictures"
+      }],
+      
       serviceWorker: null, // disable service workers
     },
   ],
