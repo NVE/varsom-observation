27432b5f3f8fe9cc072ecb49e581e675ce0b00aa
jla099 <jla099@post.uit.no>
2023-03-22 10:36:47 +0100

    small design fix

--- observasjonskort/src/components/varsom-metadata/varsom-metadata.tsx
+++ observasjonskort/src/components/varsom-metadata/varsom-metadata.tsx
@@ -43,21 +43,19 @@ export class VarsomMetadata {
       {this.moh ? 
       <span>
         <img src={getAssetPath(`/assets/icons/height-icon.svg`)}></img>
-      <label>{this.strings.RegistrationKeyInfo.MetersAboveSeaLevel} </label>
+      {this.strings.RegistrationKeyInfo.MetersAboveSeaLevel}
       {this.moh}
       </span> : ""}
 
       {this.nickname ? 
       <span>
         <img src={getAssetPath(`/assets/icons/user-icon.svg`)}></img>
-      <label>{this.strings.Reg.ObserverNickName} </label>
       {this.nickname}
       </span> : ""}
 
       {this.competenceLevelName ? 
       <span>
-      <label>{this.strings.Search.Criterias.Observers.CompetenceFilter}</label>
-      {this.competenceLevelName}
+        {this.competenceLevelName}
       </span> : ""}
      
       {this.observerGroupName ? 
