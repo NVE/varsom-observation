8b683b5436d80a9952332a1872b25c0703217818
jla099 <jla099@post.uit.no>
2023-03-24 23:02:21 +0100

    updated landslide observation component

---
+++ observasjonskort/src/components/varsom-landslide-observation/varsom-landslide-observation.stories.ts
@@ -0,0 +1,30 @@
+
+
+export default {   //source: https://ionic.io/blog/how-to-use-storybook-with-stencil
+    title: 'Components/LandslideObservation',
+}
+
+const Template = (args) => `<varsom-landslide-observation
+comment=${args.Comment}
+forecast-accurate-name=${args.ForecastAccurateName}
+damage-extent-name=${args.DamageExtentName}
+land-slide-trigger-name=${args.LandSlideTriggerName}
+land-slide-size-name=${args.LandSlideSizeName}
+land-slide-name=${args.LandSlideName}
+dt-land-slide-time=${args.DtLandSlideTime}
+dt-land-slide-time-end=${args.DtLandSlideTimeEnd}
+>
+</varsom-landslide-observation> `
+
+export const VarsomLandslideObservation = Template.bind({});
+VarsomLandslideObservation.args = {
+
+    Comment: "'Stein på FV63 løsnet fra vegskjæring 5-20m. Anslått skredvolum på veg: <1m^3.Blokkert veglengde: <10m.'",
+    ForecastAccurateName: "'det stemte bra'",
+    DamageExtentName: "'ikke spesifisert'",
+    LandSlideTriggerName: "'Ikke gitt'",
+    LandSlideSizeName: "'ikke gitt's",
+    LandSlideName: "Steinsprang",
+    DtLandSlideTime: "'2023-03-17T21:54:00+01:00'",
+    DtLandSlideTimeEnd: "'2023-03-17T21:54:00+01:00'"
+    }
