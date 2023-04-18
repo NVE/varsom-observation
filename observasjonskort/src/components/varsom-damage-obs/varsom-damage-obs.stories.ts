e83226cb58753e5bd47f643ab634fdda61dad1fa
hgr069 <hgr069@post.uit.no>
2023-04-19 00:53:54 +0200

    Created story

---
+++ observasjonskort/src/components/varsom-damage-obs/varsom-damage-obs.stories.ts
@@ -0,0 +1,29 @@
+
+
+export default {   //source: https://ionic.io/blog/how-to-use-storybook-with-stencil
+    title: 'Components/DamageObs',
+}
+
+const Template = (args) => `<varsom-damage-obs
+comment=${args.Comment}
+geo-hazard-name=${args.GeoHazardName}
+damage-type-name=${args.DamageTypeName}
+geo-hazard-t-i-d=${args.GeoHazardTID}
+damege-type-t-i-d=${args.DamageTypeTID}
+
+>
+</varsom-damage-obs> `
+
+export const VarsomDamageObs = Template.bind({});
+VarsomDamageObs.args = {
+
+    Comment: "'Kommentar til skadeobservasjon...'",
+    GeoHazardName: "Jordskred",
+    DamageTypeName: "Skade på offentlig bygg",
+    GeoHazardTID: 1,
+    DamageTypeTID: 1,
+    // DamagePosition:  
+
+
+
+    }
