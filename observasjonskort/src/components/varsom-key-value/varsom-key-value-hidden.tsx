c6e7746c2736cb16bcca79de66472c6217a32e76
hgr069 <hgr069@post.uit.no>
2023-05-07 21:56:07 +0200

    added new component

---
+++ observasjonskort/src/components/varsom-key-value/varsom-key-value-hidden.tsx
@@ -0,0 +1,37 @@
+import { Component, Prop, h } from '@stencil/core';
+
+
+@Component({
+  tag: 'varsom-key-value-hidden',
+  styleUrl: 'varsom-key-value.css',
+  shadow: true,
+  assetsDirs: ['images']
+})
+export class VarsomKeyValueHidden {
+
+  @Prop() _key?: any;
+  @Prop() _value?: any;
+  @Prop() shortVersion?: string;
+  
+  render(){
+    return <span>
+    {this.shortVersion ?    
+     <span>
+     {this._value} 
+   </span>
+   : 
+    <span>
+    <b>{this._key} -  </b>
+    {this._value}
+    </span>
+  }
+
+    </span>
+ 
+    
+  }
+    
+  } 
+
+  
+  
