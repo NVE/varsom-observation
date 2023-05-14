be5057009927f6dfce79546e5e9418940a876009
jla099 <jla099@post.uit.no>
2023-05-14 22:50:30 +0200

    published to npm and is working with Vue

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,6 +8,31 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'dist',
+      copy: [{
+        src: "**/*.i18n.*.json",
+        dest: "src/assets/i18n"
+      }, 
+      {
+        src: "**/*icon.svg",
+        dest: "src/assets/icons"
+      }, 
+      {
+        src: "**/*.png",
+        dest: "src/assets/pictures"
+      },
+      {
+        src: "**/*.jpg",
+        dest: "src/assets/pictures"
+      },
+      {
+        src: "**/ext/*.svg",
+        dest: "src/assets/svg/ext"
+      },
+      {
+        src: "**/avalanche/*.svg",
+        dest: "src/assets/svg/avalanche"
+      }      
+    ],
       esmLoaderPath: '../loader',
     },
     {
