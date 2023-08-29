b962821f5876e5686cbcbb3a95e080e9bb00d7a8
jorgenlampe <jobl@nve.no>
2023-08-29 14:24:35 +0200

    updated component to check against formatted value

--- src/components/varsom-metadata/varsom-metadata.tsx
+++ src/components/varsom-metadata/varsom-metadata.tsx
@@ -52,7 +52,7 @@ async componentWillLoad(){
       {this.registrationTimeFormatted}
       </span> : ""}
 
-      {this.dateOfLastUpdate && this.dateOfLastUpdate.value != this.dateOfRegistration.value && !this.shortVersion ? 
+      {this.dateOfLastUpdate && (this.registrationTimeFormatted != this.updateTimeFormatted) && !this.shortVersion ? 
       <span tabIndex={0}>
         <label>{this.strings.Reg.UpdatedTime} </label>
       {this.updateTimeFormatted}
